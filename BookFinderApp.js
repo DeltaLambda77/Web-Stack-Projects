502ddc59c9df9e3ee5ff8eaac6b8248a632ed003
danlozano77 <danlozano77.gmail.com>
2022-03-31 21:54:23 -0400

    scrapped old project, now book finder

---
+++ BookFinderApp.js
@@ -0,0 +1,12 @@
+/* Goals: User can enter a search query into an input field
+    User can submit the query. This will call an API that will return an
+    array of books with the corresponding data.
+    user can see the list of books appearing on the page.
+
+    Optional Goals: For each item in the list add a link that will send the
+    user to an external site which has more information about the book.
+    Implement a Responsive Design.
+    Add Loading Animations.
+ API: https://www.googleapis.com/books/v1/volumes?q=search+terms */
+
+
